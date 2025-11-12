dc77f86abfab0616d3bd3824065c7796875b63c3
Ramon-UFSJ2023 <ramonsjdr70@aluno.ufsj.edu.br>
2025-11-11 22:17:23 -0300

    SideBar sendo feita

---
+++ src/Front/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import "./Styles/NavBar.css"
+
+export default function NavBar(){
+    return(
+        <nav class="nav-bar-geral">
+            <div class="functions-nav-bar"></div>
+            <div class="functions-nav-bar"></div>
+            <div class="functions-nav-bar"></div>
+        </nav>
+    );
+}
--- src/Front/pages/HomePage.jsx
+++ src/Front/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import HeaderBar from "../components/HeaderBar";
+import NavBar from "../components/NavBar";
+import "./style/homepage.css"
 
 export default function HomePage(){
     return(
         <div className="container">
             <HeaderBar />
+            <NavBar />
         </div>
     );
 }
